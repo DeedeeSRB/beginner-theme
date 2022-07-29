15a3f10fca4e81516a84028e13d88a8580d7ac24
Sadik Besata <sadik.besata@agu.edu.tr>
2022-07-29 14:18:43 +0300

    Initial work on front page

--- front-page.php
+++ front-page.php
@@ -10,8 +10,22 @@
 get_header();
 ?>
 	<main id="primary" class="site-main container-fluid p-0">
-		<div style="height: 843px; background-color: #D3F4FF;">
-			<div>Hmmmmm</div>
+		<div class="row text-center" style="height: 843px; background-color: #304D57;">
+			<div class="my-auto pt-5">
+				<div class="title-font">BİRLİKTE YÜRÜYELİM!</div>
+				<div class="container mt-5 subtitle-font">
+					<p>WeWALK görme yetersizliğinden etkilenmiş öğrencilerin eğitim ve öğretim hizmetleri başta olmak üzere her türlü imkândan en üst düzeyde yararlanmaları için önemli bir proje başlattı. Millî Eğitim Bakanlığı tarafından sağlanan 3.500 ortaokul ve lise öğrencisine ulaşmanın hedeflendiği projenin adı “Birlikte Yürüyelim” projesi oldu.</p>
+					<p>Türkiye’nin önde gelen şirketleri, kuruluşları ve bireysel bağışçıların desteğiyle 2021 yılı içerisinde 1.000 ortaokul ve lise öğrencisi için gerekli kaynak tamamlandı. Önümüzdeki iki yılın sonunda Türkiye’de görme yetersizliği olan toplam 3.500 ortaokul ve lise öğrencisine ulaşılması hedefleniyor.</p>
+					<p>Ücretsiz dağıtılacak olan akıllı bastonlar sayesinde görme yetersizliğinden etkilenmiş öğrencilerimiz, yürürken yerdeki, göğüs ve baş hizasındaki tüm engelleri algılayabilecek. Akıllı uygulama ile istedikleri yere kolayca ve tek başlarına navigasyon ile gidebilecek. Sesli asistan sayesinde bireyler akıllı bastonlarına hangi otobüsün ne zaman geleceğini, gitmek istedikleri yerleri, etraflarında nerelerin olduğunu da sorabilecek.</p>
+					<p>WeWALK'un kurucularından Kürşat Ceylan ise “Doğuştan görme yetersizliğinden etkilenmiş birisi olarak, bağımsız hareket becerilerinin erken yaşlarda kazanılmasının ne kadar önemli olduğunu yakından biliyorum. İnsan kendisini ne kadar bağımsız ve özgür hissederse özgüveni o denli gelişiyor, hayata dair beklentileri ve heyecanı o kadar artıyor.” diye konuştu.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row text-center" style="height: 673px;">
+		</div>
+		<div class="row text-center" style="height: 560px; background: #F7F7F7; box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.25);">
+		</div>
+		<div class="row text-center" style="height: 567px;">
 		</div>
 		
 
